c4d30f571e074f7b62e68cbb21c30ca2a7df4eff
Ronit Jadhav <ronitjadhav47@gmail.com>
2023-09-27 18:51:58 +0530

    Added swaping numbers file

---
+++ 01-Num-PP/swap_a_number.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the number 1 : "))
+b = int(input("Enter the number 2:  "))
+
+# c = a 
+# a = b
+# b = c 
+
+# print(a)
+# print(b)
+
+#without using third variable
+
+a = a + b #3+4 = 7
+b = a - b # 7 - 4 = 3
+a = a - b # 7 - 3 = 4
+
+print (a)
+print(b)
